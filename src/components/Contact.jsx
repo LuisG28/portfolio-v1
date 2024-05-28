f64f069256ebf276efb1d5b2ab99330c3c722b88
LuisG28 <luis.gus2869@gmail.com>
2024-05-27 22:10:07 -0600

    Change colors of text and emoji of contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import TagTittle from './TagTittle'
 const Contact = () => {
   return (
     <div id="contact" className="w-full flex flex-col items-center justify-center mb-20">
-      <TagTittle  tittle=" Contactame"/>
+      <TagTittle  tittle="☎️ Contactame"/>
       <h1 className="text-6xl mt-5 text-white font-bold">¡Vamos a conversar!</h1>
       <div className="flex flex-row w-full items-center justify-center mt-10 space-x-3">
         <a href="https://wa.me/+522214172669?text=Hola%20quiero%20más%20información" target="_blank" className="text-white border-tertiary w-64 h-16 rounded-lg p-1 text-lg hover:text-lg border-2 px-6 py-3 my-2 flex hover:text-base items-center hover:bg-tertiary hover:border-tertiary">
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -44,16 +44,14 @@ const Skills = () => {
       <ol className="mt-10">
         {
           jobs.map((item, index)=> (
-            <li key={index} className={`border-l-2 lg:mt-0 mt-[-35px] ${item.isActual ?  'border-[#2BB8E4]' : 'border-purple-600'}`}>
+            <li key={index} className={`border-l-2 lg:mt-0 mt-[-35px] ${item.isActual ?  'border-[#2BB8E4]' : 'border-tertiary'}`}>
               <div className="md:flex flex-start">
-                <div className={"${item.isActual ?  'border-[#2BB8E4]' : 'border-purple-600'} w-6 h-6 flex items-center justify-center rounded-full -ml-3.5"}>
-                  <svg aria-hidden="true" focusable="false" data-prefix="fas" className="text-white w-3 h-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <path fill="currentColor" d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm64-192c0-8.8 7.2-16 16-16h288c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16v-64zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"></path>
-                  </svg>
+                <div className={`${item.isActual ?  'border-[#2BB8E4]' : 'border-purple-600'} w-6 h-6 flex items-center justify-center rounded-full -ml-3.5`}>
+                  🗓️
                 </div>
                 <div className="block p-6 rounded-lg shadow-lg max-w-md ml-6 mb-10 border-2 border-[#2BB8E4]">
                   <div className="flex justify-between mb-4">
-                    <span className="font-medium text-white hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">{item.name}</span>
+                    <span className="font-medium text-white hover:text-tertiary focus:text-tertiary duration-300 transition ease-in-out text-sm">{item.name}</span>
                   </div>
                   <p className="text-white mb-6">{item.description}</p>
                   <div className="flex flex-row justify-between">
