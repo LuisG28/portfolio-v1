43a657d76f8e9feb8ecfeb087447c3beaadf0886
Luis Gustavo <luis.gus2869@gmail.com>
2024-06-27 12:03:28 -0600

    remove img and center section about mee

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,7 +3,7 @@ import TagTittle from "./TagTittle"
 
 export default function AboutMe () {
     return (
-        <motion.article 
+        <motion.section 
             initial="hidden" 
             whileInView="visible" 
             viewport={{once: true, amount: 0.5}}
@@ -12,27 +12,18 @@ export default function AboutMe () {
                 hidden: { opacity: 0, y: 50 }, 
                 visible: { opacity: 1, y: 0 },
             }}
-            className="w-full flex flex-col-reverse lg:flex-row justify-center lg:space-x-64 items-center"
+            className="w-full flex flex-col items-center justify-center"
             id="about"
         >
-            <img 
-                src={'../../avatar.jpeg'}
-                alt="avatar image" 
-                className='rounded-full w-40 h-40 lg:h-auto lg:w-96 mt-5 lg:mt-0' 
-                loading="lazy"
-                
-            />
-            <section className="h-auto flex flex-col justify-center items-center lg:items-stretch lg:w-4/6 lg:space-y-2 w-full lg:p-0 px-10 space-y-5">
-                <div className="w-44">
-                    <TagTittle tittle="🧐 Sobre mi"/>
-                </div>
-                <label className="lg:text-6xl text-2xl text-white font-bold w-auto">Luis Gustavo Cruz Rosales.</label>
+            <TagTittle tittle="🧐 Sobre mi"/>
+            <h1 className="lg:text-6xl text-2xl text-white font-bold w-auto">Luis Gustavo Cruz Rosales.</h1>
+            <section className="h-auto flex flex-col lg:justify-center lg:items-center lg:space-y-2 w-full lg:p-0 space-y-5 mt-10 lg:mt-5">
                 <p className="text-white lg:text-lg text-sm"><span className="lg:text-2xl text-sm">👋</span> Me llamo Luis Gustavo Cruz Rosalez, pero puedes llamarme simplemente Gustavo, un placer!</p>
                 <p className="text-white lg:text-lg text-sm"><span className="lg:text-2xl text-sm">🧑‍💻</span> Llevo más de 3 años trabajando como desarrollador full stack.</p>
                 <p className="text-white lg:text-lg text-sm"><span className="lg:text-2xl text-sm">👨‍🎓</span> Graduado en tecnologías de la información y comunicación por la universidad técnologica de Xicotepec de Jurarez.</p>
                 <p className="text-white lg:text-lg text-sm"><span className="lg:text-2xl text-sm">💡</span> Interesado en el desarrolo de aplicaciones web y moviles.</p>
             </section>
-        </motion.article>
+        </motion.section>
     )
 }
 
