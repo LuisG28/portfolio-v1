f62c1f442c8efd99152b6ad61eb5dfaf39e19076
Luis Gustavo <luis.gus2869@gmail.com>
2024-09-02 21:37:21 -0600

    To adjust header

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,10 +7,6 @@ const Contact = () => {
       <TagTittle  tittle="☎️ Contactame"/>
       <h1 className="lg:text-6xl text-2xl mt-5 text-white font-bold">¡Vamos a conversar!</h1>
       <div className="flex flex-row w-full items-center justify-center mt-10 space-x-3">
-        <a href="https://wa.me/+522214172669?text=Hola%20quiero%20más%20información" target="_blank" className="text-white border-tertiary w-64 h-16 rounded-lg p-1 text-lg hover:text-lg border-2 px-6 py-3 my-2 flex hover:text-base items-center hover:bg-tertiary hover:border-tertiary">
-          <i className="fa-brands fa-whatsapp mr-3" /> 
-          Vamos a conversar
-        </a>
         <a href="mailto:luis.gus2869@gmail.com?subject=Consulta&body=Hola,%20quiero%20más%20información" target="_blank" className="text-white border-tertiary w-64 h-16 rounded-lg p-1 text-lg hover:text-lg border-2 px-6 py-3 my-2 flex hover:text-base items-center hover:bg-tertiary hover:border-tertiary">
           <i className="fa-regular fa-envelope mr-3" /> 
           Correo
