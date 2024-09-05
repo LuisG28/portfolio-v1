526e94ddb58b46e016730e36218bba00bd403fb1
Luis Gustavo <luis.gus2869@gmail.com>
2024-09-05 00:08:35 -0600

    Change styles in tags and update images by webp

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,16 +16,14 @@ export default function Heade({ dataLanguage }) {
             className="w-full h-auto flex flex-col lg:flex-row justify-center items-center lg:space-x-64"
         >
             <section className="h-auto w-96 flex flex-col justify-center items-center lg:items-baseline space-y-1 lg:space-x-0" id="home">
-                <div className="w-44">
-                    <TagTittle tittle={dataLanguage.hi}/>
-                </div>
+                <TagTittle tittle={dataLanguage.hi}/>
                 <label className="lg:text-6xl text-2xl text-white font-bold w-auto pt-5">Luis Gustavo</label>
                 <span className="text-white font-thin opacity-50">Full Stack Developer</span>
                 <div className="space-x-1">
                     <a href="https://github.com/LuisG28" target="_blank" className="fab fa-github text-2xl text-gray-500 cursor-pointer hover:text-white"/>
                     <a href="https://www.linkedin.com/in/luis-gustavo-cruz-rosales-1383351a1/" target="_blank" className="fa-brands fa-linkedin text-2xl text-gray-500 cursor-pointer hover:text-white"/>
                 </div>
-                <a href="/portfolio-v1/LuisGustavoCruzCV.pdf" download="LuisGustavoCruzCV.pdf" className="text-white border-tertiary w-48 group border-2 px-6 py-3 my-2 flex hover:text-base items-center hover:bg-tertiary hover:border-tertiary">
+                <a href="/portfolio-v1/LuisGustavoCruzCV.pdf" download="LuisGustavoCruzCV.pdf" className="text-white border-tertiary w-auto group border-2 px-6 py-3 my-2 flex hover:text-base items-center hover:bg-tertiary hover:border-tertiary">
                     {dataLanguage.resume}
                     <span className="group-hover:rotate-90 duration-300">
                     <i className="fa-solid fa-arrow-right ml-5"/>
@@ -33,7 +31,7 @@ export default function Heade({ dataLanguage }) {
                 </a>
             </section>
             <img 
-                src={'/portfolio-v1/avatar.png'}
+                src={'/portfolio-v1/avatar.webp'}
                 alt="avatar image" 
                 className='rounded-full w-40 h-40 lg:h-72 lg:w-72 mt-5 lg:mt-0' 
                 loading="lazy"
--- src/components/TagTittle.jsx
+++ src/components/TagTittle.jsx
@@ -1,6 +1,6 @@
 export default function TagTittle ({tittle}) {
     return (
-        <div className="bg-primary w-auto text-2xl tracking-widest text-white px-2 py-1 rounded-lg font-medium">
+        <div className="bg-primary w-auto text-2xl tracking-widest text-white px-2 py-1 rounded-lg font-medium flex justify-center">
             <span className=" text-2xl tracking-widest text-white font-medium">{tittle}</span>
         </div>
     )
